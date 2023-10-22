215b536c702a6408451a2e0ccc48a5f9992acf63
Abdul Matheen <mateen16@gmail.com>
2023-10-21 23:17:51 -0400

    commit

--- Class/Flask/app.py
+++ Class/Flask/app.py
@@ -2,8 +2,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/add')
-def addition(a,b):
-    return a+b
+# @app.route('/add')
+# def addition(a,b):
+#     return a+b
 
-print(addition(10,3))
+@app.route('/home')
+def homepage():
+    return "Welcome to Flask"
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
